ab147e0d52dea9fc2f207850598e6c4eb32cbdc5
Matt Hammond <mjhammond94@gmail.com>
2023-04-26 16:53:33 +0100

    app.rb file updated - recipes successfully printed to terminal

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require_relative 'lib/recipe_repository'
 
 DatabaseConnection.connect('recipes_directory')
 
+# repo = RecipeRepository.new
+# recipe = repo.find(5)
+# puts recipe.name
+
 repo = RecipeRepository.new
-recipe = repo.find(5)
-puts recipe.name
+recipes = repo.all
+recipes.each do |recipe|
+  puts recipe.name
+end
